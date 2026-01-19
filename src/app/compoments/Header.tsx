04cae7df5b2b8307dd55c4314a5e0047915a5910
Tung, Ta Van (Hitex)_ <tavantung268@gmail.com>
2026-01-19 22:00:51 +0700

    Fix: Add targetDate prop to Countdown in Header component

--- src/app/compoments/Header.tsx
+++ src/app/compoments/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center px-6 py-4 bg-pink-700 shadow-md fixed top-0 left-0 right-0 z-50">
       {/* Countdown bên trái */}
-      <Countdown />
+      <Countdown targetDate={new Date('2026-03-08')} />
 
       {/* Music player bên phải */}
       <MusicPlayer />
